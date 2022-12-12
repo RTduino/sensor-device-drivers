ff6fef46c4c44773155a6ba203bbb3e0d87974d8
Meco Man <920369182@qq.com>
2022-12-12 00:38:32 -0500

    sync 5.0.0 sensor framework

--- Adafruit-AHTx0/sensor_adafruit_ahtx0.cpp
+++ Adafruit-AHTx0/sensor_adafruit_ahtx0.cpp
@@ -22,7 +22,7 @@
 
 static Adafruit_AHTX0 aht;
 
-static rt_err_t _ahtx0_init(void)
+static rt_err_t adafruit_ahtx0_init(void)
 {
     if (!aht.begin())
     {
@@ -31,7 +31,7 @@ static rt_err_t _ahtx0_init(void)
     return RT_EOK;
 }
 
-static rt_ssize_t _ahtx0_polling_get_data(rt_sensor_t sensor, rt_sensor_data_t data)
+static rt_ssize_t adafruit_ahtx0_polling_get_data(rt_sensor_t sensor, rt_sensor_data_t data)
 {
     sensors_event_t humidity, temp;
     aht.getEvent(&humidity, &temp); /* populate temp and humidity objects with fresh data */
@@ -54,14 +54,13 @@ static rt_ssize_t _ahtx0_polling_get_data(rt_sensor_t sensor, rt_sensor_data_t d
     }
 }
 
-
-static rt_ssize_t ahtx0_fetch_data(rt_sensor_t sensor, rt_sensor_data_t buf, rt_size_t len)
+static rt_ssize_t adafruit_ahtx0_fetch_data(rt_sensor_t sensor, rt_sensor_data_t buf, rt_size_t len)
 {
     RT_ASSERT(buf);
 
     if (sensor->config.mode == RT_SENSOR_MODE_POLLING)
     {
-        return _ahtx0_polling_get_data(sensor, buf);
+        return adafruit_ahtx0_polling_get_data(sensor, buf);
     }
     else
     {
@@ -69,8 +68,7 @@ static rt_ssize_t ahtx0_fetch_data(rt_sensor_t sensor, rt_sensor_data_t buf, rt_
     }
 }
 
-
-static rt_err_t ahtx0_control(rt_sensor_t sensor, int cmd, void *args)
+static rt_err_t adafruit_ahtx0_control(rt_sensor_t sensor, int cmd, void *args)
 {
     rt_err_t result = -RT_EINVAL;
 
@@ -79,8 +77,8 @@ static rt_err_t ahtx0_control(rt_sensor_t sensor, int cmd, void *args)
 
 static struct rt_sensor_ops sensor_ops =
 {
-    ahtx0_fetch_data,
-    ahtx0_control
+    adafruit_ahtx0_fetch_data,
+    adafruit_ahtx0_control
 };
 
 static const char *sensor_name = "aht10/20";
@@ -90,7 +88,6 @@ int rt_hw_ahtx0_init(void)
     rt_int8_t result;
     rt_sensor_t sensor_temp = RT_NULL, sensor_humi = RT_NULL;
 
-
      /* temperature sensor register */
     sensor_temp = (rt_sensor_t)rt_calloc(1, sizeof(struct rt_sensor_device));
     if (sensor_temp == RT_NULL)
@@ -99,7 +96,7 @@ int rt_hw_ahtx0_init(void)
     sensor_temp->info.type       = RT_SENSOR_CLASS_TEMP;
     sensor_temp->info.vendor     = RT_SENSOR_VENDOR_ASAIR;
     sensor_temp->info.model      = sensor_name;
-    sensor_temp->info.unit       = RT_SENSOR_UNIT_DCELSIUS;
+    sensor_temp->info.unit       = RT_SENSOR_UNIT_CELSIUS;
     sensor_temp->info.intf_type  = RT_SENSOR_INTF_I2C;
     sensor_temp->info.range_max  = SENSOR_TEMP_RANGE_MAX;
     sensor_temp->info.range_min  = SENSOR_TEMP_RANGE_MIN;
@@ -125,7 +122,7 @@ int rt_hw_ahtx0_init(void)
     sensor_humi->info.type       = RT_SENSOR_CLASS_HUMI;
     sensor_humi->info.vendor     = RT_SENSOR_VENDOR_ASAIR;
     sensor_humi->info.model      = sensor_name;
-    sensor_humi->info.unit       = RT_SENSOR_UNIT_PERMILLAGE;
+    sensor_humi->info.unit       = RT_SENSOR_UNIT_PERCENTAGE;
     sensor_humi->info.intf_type  = RT_SENSOR_INTF_I2C;
     sensor_humi->info.range_max  = SENSOR_HUMI_RANGE_MAX;
     sensor_humi->info.range_min  = SENSOR_HUMI_RANGE_MIN;
@@ -141,7 +138,7 @@ int rt_hw_ahtx0_init(void)
         goto __exit;
     }
 
-    _ahtx0_init();
+    adafruit_ahtx0_init();
     return RT_EOK;
 
 __exit:

b21642a430d35af7d9f8886ef5221096705c7712
Meco Man <920369182@qq.com>
2022-12-22 16:22:19 -0500

    update adafruit-ahtx0 for new sensor framework

--- Adafruit-AHTx0/sensor_adafruit_ahtx0.cpp
+++ Adafruit-AHTx0/sensor_adafruit_ahtx0.cpp
@@ -15,11 +15,6 @@
 #define DBG_LVL DBG_INFO
 #include <rtdbg.h>
 
-#define SENSOR_TEMP_RANGE_MAX (85)
-#define SENSOR_TEMP_RANGE_MIN (-40)
-#define SENSOR_HUMI_RANGE_MAX (100)
-#define SENSOR_HUMI_RANGE_MIN (0)
-
 static Adafruit_AHTX0 aht;
 
 static rt_err_t adafruit_ahtx0_init(void)
@@ -36,13 +31,13 @@ static rt_ssize_t adafruit_ahtx0_polling_get_data(rt_sensor_t sensor, rt_sensor_
     sensors_event_t humidity, temp;
     aht.getEvent(&humidity, &temp); /* populate temp and humidity objects with fresh data */
 
-    if (sensor->info.type == RT_SENSOR_CLASS_TEMP)
+    if (sensor->info.type == RT_SENSOR_TYPE_TEMP)
     {
         data->data.temp = temp.temperature;
         data->timestamp = rt_sensor_get_ts();
         return 1;
     }
-    else if (sensor->info.type == RT_SENSOR_CLASS_HUMI)
+    else if (sensor->info.type == RT_SENSOR_TYPE_HUMI)
     {
         data->data.humi = humidity.relative_humidity;
         data->timestamp = rt_sensor_get_ts();
@@ -58,7 +53,7 @@ static rt_ssize_t adafruit_ahtx0_fetch_data(rt_sensor_t sensor, rt_sensor_data_t
 {
     RT_ASSERT(buf);
 
-    if (sensor->config.mode == RT_SENSOR_MODE_POLLING)
+    if (RT_SENSOR_MODE_GET_FETCH(sensor->info.mode) == RT_SENSOR_MODE_FETCH_POLLING)
     {
         return adafruit_ahtx0_polling_get_data(sensor, buf);
     }
@@ -81,28 +76,38 @@ static struct rt_sensor_ops sensor_ops =
     adafruit_ahtx0_control
 };
 
-static const char *sensor_name = "aht10/20";
+static const char *sensor_name = "ahtx0";
 
-int rt_hw_ahtx0_init(void)
+static int rt_hw_ahtx0_init(void)
 {
     rt_int8_t result;
     rt_sensor_t sensor_temp = RT_NULL, sensor_humi = RT_NULL;
+
+    if (adafruit_ahtx0_init() != RT_EOK)
+        return -1;
 
      /* temperature sensor register */
     sensor_temp = (rt_sensor_t)rt_calloc(1, sizeof(struct rt_sensor_device));
     if (sensor_temp == RT_NULL)
         return -1;
 
-    sensor_temp->info.type       = RT_SENSOR_CLASS_TEMP;
+    sensor_temp->info.type       = RT_SENSOR_TYPE_TEMP;
     sensor_temp->info.vendor     = RT_SENSOR_VENDOR_ASAIR;
-    sensor_temp->info.model      = sensor_name;
+    sensor_temp->info.name       = sensor_name;
     sensor_temp->info.unit       = RT_SENSOR_UNIT_CELSIUS;
     sensor_temp->info.intf_type  = RT_SENSOR_INTF_I2C;
-    sensor_temp->info.range_max  = SENSOR_TEMP_RANGE_MAX;
-    sensor_temp->info.range_min  = SENSOR_TEMP_RANGE_MIN;
-    sensor_temp->info.period_min = 5;
 
-    //rt_memcpy(&sensor_temp->config, cfg, sizeof(struct rt_sensor_config));
+    sensor_temp->info.acquire_min = 1000;
+    sensor_temp->info.accuracy.resolution = 0.01;
+    sensor_temp->info.accuracy.error = 0.3;
+    sensor_temp->info.scale.range_min = -40.0;
+    sensor_temp->info.scale.range_max = 85.0;
+
+    sensor_temp->config.intf.dev_name = Wire._i2c_bus_dev->parent.parent.name;
+    sensor_temp->config.intf.type = RT_SENSOR_INTF_I2C;
+    sensor_temp->config.intf.arg = RT_NULL;
+    sensor_temp->config.irq_pin.pin = RT_PIN_NONE;
+
     sensor_temp->ops = &sensor_ops;
 
     result = rt_hw_sensor_register(sensor_temp, sensor_name, RT_DEVICE_FLAG_RDONLY, RT_NULL);
@@ -119,16 +124,23 @@ int rt_hw_ahtx0_init(void)
         return -1;
     }
 
-    sensor_humi->info.type       = RT_SENSOR_CLASS_HUMI;
+    sensor_humi->info.type       = RT_SENSOR_TYPE_HUMI;
     sensor_humi->info.vendor     = RT_SENSOR_VENDOR_ASAIR;
-    sensor_humi->info.model      = sensor_name;
+    sensor_humi->info.name       = sensor_name;
     sensor_humi->info.unit       = RT_SENSOR_UNIT_PERCENTAGE;
     sensor_humi->info.intf_type  = RT_SENSOR_INTF_I2C;
-    sensor_humi->info.range_max  = SENSOR_HUMI_RANGE_MAX;
-    sensor_humi->info.range_min  = SENSOR_HUMI_RANGE_MIN;
-    sensor_humi->info.period_min = 5;
 
-    //rt_memcpy(&sensor_humi->config, cfg, sizeof(struct rt_sensor_config));
+    sensor_humi->info.acquire_min = 1000;
+    sensor_humi->info.accuracy.resolution = 0.024;
+    sensor_humi->info.accuracy.error = 2.0;
+    sensor_humi->info.scale.range_min = 0.0;
+    sensor_humi->info.scale.range_max = 100.0;
+
+    sensor_humi->config.intf.dev_name = Wire._i2c_bus_dev->parent.parent.name;
+    sensor_humi->config.intf.type = RT_SENSOR_INTF_I2C;
+    sensor_humi->config.intf.arg = RT_NULL;
+    sensor_humi->config.irq_pin.pin = RT_PIN_NONE;
+
     sensor_humi->ops = &sensor_ops;
 
     result = rt_hw_sensor_register(sensor_humi, sensor_name, RT_DEVICE_FLAG_RDONLY, RT_NULL);
@@ -138,7 +150,6 @@ int rt_hw_ahtx0_init(void)
         goto __exit;
     }
 
-    adafruit_ahtx0_init();
     return RT_EOK;
 
 __exit:
